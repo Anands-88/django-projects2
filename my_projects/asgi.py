5ac90248d35788b03cd0d3e0d569e321c807a869
Anands-88 <anandsholla8@gmail.com>
2021-07-30 22:59:19 +0530

    Add updated files

--- my_projects/asgi.py
+++ my_projects/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for django_projects project.
+ASGI config for my_projects project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_projects.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'my_projects.settings')
 
 application = get_asgi_application()
