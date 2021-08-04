1515223f62b26f282c4167891dc22579860cb563
Anands-88 <anandsholla8@gmail.com>
2021-08-04 16:09:16 +0530

    Add updated files

--- school/models.py
+++ school/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Parent(models.Model):
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    phone = models.CharField(max_length=20)
+
+class Contribution(models.Model):
+    Type = models.CharField(max_length=20)
+    parent = models.ManyToManyField(Parent)
+    
